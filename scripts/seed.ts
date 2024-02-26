53a65e5139b1a5792bdea4a8d8a6abfe7afc8114
Brian Gates <brian.g.gates@gmail.com>
2024-02-25 19:51:35 -0800

    add postgres db, prisma, seed

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+import prisma from "../lib/prisma";
+
+export async function seed() {
+    console.time('seed')
+    await prisma.card.create({
+        data: {
+            id: 'strike',
+            title: 'Strike'
+        },
+    })
+    debugger
+    await prisma.card.create({
+        data: {
+            id: 'defend',
+            title: 'Defend'
+        }
+    })
+    await prisma.card.create({
+        data: {
+            id: 'bash',
+            title: 'Bash'
+        }
+    })
+    console.timeEnd('seed')
+}
+
+seed()
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import prisma from "../../lib/prisma";
+import { Card } from "@prisma/client";
+
 export default function Board() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-violet-100 text-slate-800">
@@ -10,52 +13,49 @@ export default function Board() {
   );
 }
 
-function PlayerHand() {
-  async function playCard(...args: any[]) {
+async function PlayerHand() {
+  const cards = await prisma.card.findMany({
+    take: 4
+  })
+  
+  async function playCard(formData: FormData) {
     'use server';
-    console.log("play card", args);
+    console.log("play card", formData);
   }
   return (
     <form action={playCard} className="flex flex-row gap-1">
-      <Card id="strike" title="Strike" description="Deal 6 damage." />
-      <Card id="defend" title="Defend" description="Gain 5 block." />
-      <Card id="bash" title="Bash" description="Deal 8 damage and apply 2 vulnerable." />
-      <Card id="flex" title="Flex" description="Gain 2 strength." />
+      {cards.map((card => {
+        return <Card {...card} />
+      }))}
     </form>
   );
 }
 
 function Card({
   title,
-  description,
   id,
-}: {
-  title: string;
-  description: string;
-  id: string;
-}) {
+}: Card) {
   return (
     <button
       name="action"
       value={id}
       type="submit"
-      className="w-36 h-56 bg-slate-100 rounded-md shadow-md border-2 hover:border-2 hover:border-blue-500 hover:scale-125 transition cursor-pointer"
+      className="w-36 h-56 bg-slate-100 rounded-md shadow-md border-2 hover:border-2 hover:scale-125 transition cursor-pointer flex flex-col"
     >
-      <h4 className="text-lg font-bold text-center">{title}</h4>
-      <div className="flex flex-col gap-1">
-        <div className="p-1">
-          <div className="h-20 w-full bg-slate-300 self-center rounded-sm shadow-inner"></div>
-        </div>
-        <p className="text-sm p-2">{description}</p>
+      <h4 className="text-lg font-bold text-center border-b w-full">{title}</h4>
+      <div className="p-1 grow gap-1 flex flex-col">
+        <div className="h-20 w-full bg-slate-300 self-center rounded-sm shadow-inner"></div>
+        <p className="text-sm p-2 text-left shadow-inner rounded-sm h-fit border-2 border-green-300 grow">Description goes here.</p>
       </div>
     </button>
   );
 }
 
-function PlayerAvatar() {
+async function PlayerAvatar() {
+  const player = await prisma.player.findFirst()
   return (
     <div className="flex flex-col gap-1">
-      <Stats />
+      <Stats health={player?.health ?? 10} maxHealth={player?.maxHealth ?? 10} />
     </div>
   );
 }
@@ -63,20 +63,38 @@ function PlayerAvatar() {
 function EnemyAvatar() {
   return (
     <div className="flex flex-col gap-1">
-      <Stats />
+      <Stats health={10} maxHealth={10} />
     </div>
   );
 }
 
-function Stats() {
+function Stats({
+  health,
+  maxHealth
+}: {
+  health: number,
+  maxHealth: number
+}) {
+  const percentHealth = Math.floor(health / maxHealth)
   return (
     <div className="flex flex-row gap-1">
-      <div className="w-24 h-4 bg-red-500 rounded-md overflow-hidden">
-        <div className="bg-gradient-to-b from-green-400 to-green-700 w-full h-full"></div>
+      <div className="w-24 h-4 bg-gradient-to-b to-red-400 from-red-700 rounded-md overflow-hidden">
+        <div className={`bg-gradient-to-b from from-green-400 to-green-700 w-[${percentHealth}%] h-full shadow-2xl`}></div>
       </div>
       <div className="w-24 h-4 bg-blue-500 rounded-md overflow-hidden">
         <div className="bg-gradient-to-b from-blue-400 to-blue-700 w-full h-full"></div>
       </div>
     </div>
   );
+}
+
+function EndTurn() {
+  async function endTurn() {
+    'use server'
+  }
+  return <button
+  name="action"
+  value='endTurn'
+  type="submit"
+>End Turn</button>
 }
