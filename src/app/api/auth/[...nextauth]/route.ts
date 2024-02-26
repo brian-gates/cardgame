26acd87802ef4d6cbc255699263db09e1f39c997
Brian Gates <brian.g.gates@gmail.com>
2024-02-26 13:16:39 -0800

    working login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,18 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID ?? "",
+      clientSecret: process.env.DISCORD_SECRET ?? "",
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
--- src/app/lib/login-button.tsx
+++ src/app/lib/login-button.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { PiSignIn, PiSignOut } from "react-icons/pi";
 
 export function LoginButton() {
   const session = useSession();
@@ -9,21 +10,45 @@ export function LoginButton() {
       case "authenticated":
         return "Authenticated";
       case "loading":
-        return "Auth status...";
+        return "Loading...";
       case "unauthenticated":
-        return "Login";
+        return (
+          <div className="flex flex-row gap-2 items-center">
+            <PiSignIn />
+            <span>Sign in</span>
+          </div>
+        );
       default:
         return "Unknown";
     }
   }
 
   if (session.data) {
-    return <button onClick={() => signOut()}>Sign Out</button>;
+    return (
+      <div className="flex flex-row gap-3 items-center p-3 rounded-md">
+        <div className="rounded-full overflow-hidden w-8 h-8">
+          <img src={session.data.user?.image ?? ""} alt="user avatar" />
+        </div>
+        <div className="text-sm">Signed in as {session.data.user?.name}</div>
+        <button
+          className="flex flex-row gap-2 items-center border border-gray-300 px-2 py-1 rounded-md"
+          onClick={() => signOut()}
+        >
+          <PiSignOut />
+          <span>Sign Out</span>
+        </button>
+      </div>
+    );
   }
 
   return (
-    <button onClick={() => signIn()}>
-      <Text status={session.status} />
-    </button>
+    <div className="flex flex-row gap-3 items-center p-3 rounded-md">
+      <button
+        className="flex flex-row gap-2 items-center border border-gray-300 px-2 py-1 rounded-md"
+        onClick={() => signIn()}
+      >
+        <Text status={session.status} />
+      </button>
+    </div>
   );
 }
