a8a3c2d19c16d2791dd2cb4e6a16c16eb66e7caf
Brian Gates <brian.g.gates@gmail.com>
2024-02-26 22:44:26 -0800

    progress

--- src/app/lib/cards/Strike.tsx
+++ src/app/lib/cards/Strike.tsx
@@ -3,26 +3,13 @@ import { CardProps } from ".";
 import { revalidatePath } from "next/cache";
 import { Card } from "./Card";
 import { discard } from "../actions/discard";
+import { damageEnemy } from "../actions/damageEnemy";
 
 export function Strike({ card }: CardProps) {
   const damage = 5;
   async function execute() {
     "use server";
-    const session = await getServerSession();
-    const id = session?.user?.email;
-    if (!id) return;
-    const player = await prisma.player.findUnique({
-      where: { id },
-      include: {
-        encounter: true,
-      },
-    });
-    const enemy = player?.encounter;
-    if (!enemy) return;
-    await prisma.enemy.update({
-      where: { id: enemy.id },
-      data: { health: Math.max(enemy.health - damage, 0) },
-    });
+    await damageEnemy(damage);
     await discard(card.id);
     revalidatePath("/");
   }
