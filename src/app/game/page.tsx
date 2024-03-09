45759ef1cdfa6498bbafc0cf69ef3dbc6009544d
Brian Gates <brian.g.gates@gmail.com>
2024-03-08 18:41:12 -0800

    doc links in .env, game redirects to / if no session

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,7 +1,12 @@
 import { redirect } from "next/navigation";
 import { getEnemy } from "../_lib/enemies/getEnemy";
+import { getServerSession } from "next-auth";
 
 export default async function GamePage() {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/");
+  }
   const enemy = await getEnemy();
   if (enemy) {
     redirect("/game/combat");
